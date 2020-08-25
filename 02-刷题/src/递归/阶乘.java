4f9577b34a91841562ee8af4c245c6ada36efa51
zongqi <1165064143@qq.com>
2020-08-25 10:03:24 +0800

    迷宫问题

---
+++ 02-刷题/src/递归/阶乘.java
@@ -0,0 +1,17 @@
+package 递归;
+
+public class 阶乘 {
+
+    /**
+     * 阶乘
+     * @param n
+     * @return
+     */
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+}
