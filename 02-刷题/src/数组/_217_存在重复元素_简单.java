e5f2c637d6ff99e950f5220780ab0132b6a8d723
zongqi <1165064143@qq.com>
2020-08-28 09:49:00 +0800

    刷题

---
+++ 02-刷题/src/数组/_217_存在重复元素_简单.java
@@ -0,0 +1,51 @@
+package 数组;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 217. 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ * 示例:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ */
+public class _217_存在重复元素_简单 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        boolean b = new _217_存在重复元素_简单().containsDuplicate(nums);
+        System.out.println(b);
+    }
+
+
+    /**
+     *
+     * 哈希碰撞
+     */
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>(1024);
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 暴力 双循环, 超时 --不推荐
+     */
+    public boolean containsDuplicateV1(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
