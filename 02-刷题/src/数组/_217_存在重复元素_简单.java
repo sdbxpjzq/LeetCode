de15719428a95b744aba2142d17f7f2df0400ed8
zongqi <1165064143@qq.com>
2020-08-28 19:37:00 +0800

    刷题

--- 02-刷题/src/数组/_217_存在重复元素_简单.java
+++ 02-刷题/src/数组/_217_存在重复元素_简单.java
@@ -33,19 +33,4 @@ public class _217_存在重复元素_简单 {
         }
         return false;
     }
-
-
-    /**
-     * 暴力 双循环, 超时 --不推荐
-     */
-    public boolean containsDuplicateV1(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[i] == nums[j] && i != j) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 }
