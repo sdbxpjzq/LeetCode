24ccf53c851cc480f3acd0a7c1e15f8b91e80803
zongqi <1165064143@qq.com>
2020-09-03 09:40:24 +0800

    顺序存储二叉树

---
+++ 01-基础思想/src/树/二叉树/前中后序遍历/BinaryTreeDemo.java
@@ -0,0 +1,159 @@
+package 树.二叉树.前中后序遍历;
+
+public class BinaryTreeDemo {
+
+    public static void main(String[] args) {
+        //先需要创建一颗二叉树
+        BinaryTree binaryTree = new BinaryTree();
+        //创建需要的结点
+        Node root = new Node(1, "宋江");
+        Node node2 = new Node(2, "吴用");
+        Node node3 = new Node(3, "卢俊义");
+        Node node4 = new Node(4, "林冲");
+        Node node5 = new Node(5, "关胜");
+
+        //说明，我们先手动创建该二叉树，后面我们学习递归的方式创建二叉树
+        root.setLeft(node2);
+        root.setRight(node3);
+        node3.setRight(node4);
+        node3.setLeft(node5);
+        binaryTree.setRoot(root);
+
+        //测试
+		System.out.println("前序遍历"); // 1,2,3,5,4
+		binaryTree.preOrder();
+
+        //测试
+//		System.out.println("中序遍历");
+//		binaryTree.infixOrder(); // 2,1,5,3,4
+//
+//		System.out.println("后序遍历");
+//		binaryTree.postOrder(); // 2,5,4,3,1
+
+        //前序遍历
+        //前序遍历的次数 ：4
+//		System.out.println("前序遍历方式~~~");
+//		HeroNode resNode = binaryTree.preOrderSearch(5);
+//		if (resNode != null) {
+//			System.out.printf("找到了，信息为 no=%d name=%s", resNode.getNo(), resNode.getName());
+//		} else {
+//			System.out.printf("没有找到 no = %d 的英雄", 5);
+//		}
+
+        //中序遍历查找
+        //中序遍历3次
+//		System.out.println("中序遍历方式~~~");
+//		HeroNode resNode = binaryTree.infixOrderSearch(5);
+//		if (resNode != null) {
+//			System.out.printf("找到了，信息为 no=%d name=%s", resNode.getNo(), resNode.getName());
+//		} else {
+//			System.out.printf("没有找到 no = %d 的英雄", 5);
+//		}
+
+        //后序遍历查找
+        //后序遍历查找的次数  2次
+//		System.out.println("后序遍历方式~~~");
+//		HeroNode resNode = binaryTree.postOrderSearch(5);
+//		if (resNode != null) {
+//			System.out.printf("找到了，信息为 no=%d name=%s", resNode.getNo(), resNode.getName());
+//		} else {
+//			System.out.printf("没有找到 no = %d 的英雄", 5);
+//		}
+
+        //测试一把删除结点
+
+        System.out.println("删除前,前序遍历");
+        binaryTree.preOrder(); //  1,2,3,5,4
+        binaryTree.delNode(5);
+        //binaryTree.delNode(3);
+        System.out.println("删除后，前序遍历");
+        binaryTree.preOrder(); // 1,2,3,4
+
+
+
+    }
+
+}
+
+//定义BinaryTree 二叉树
+class BinaryTree {
+    private Node root;
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    //删除结点
+    public void delNode(int no) {
+        if(root != null) {
+            //如果只有一个root结点, 这里立即判断root是不是就是要删除结点
+            if(root.getNo() == no) {
+                root = null;
+            } else {
+                //递归删除
+                root.delNode(no);
+            }
+        }else{
+            System.out.println("空树，不能删除~");
+        }
+    }
+
+    /**
+     * 前序遍历
+     */
+    public void preOrder() {
+        if(this.root != null) {
+            this.root.前序遍历();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void infixOrder() {
+        if(this.root != null) {
+            this.root.中序遍历();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    /**
+     * 后序遍历
+     */
+    public void postOrder() {
+        if(this.root != null) {
+            this.root.后序遍历();
+        }else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    //前序遍历
+    public Node preOrderSearch(int no) {
+        if(root != null) {
+            return root.preOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+    //中序遍历
+    public Node infixOrderSearch(int no) {
+        if(root != null) {
+            return root.infixOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+    //后序遍历
+    public Node postOrderSearch(int no) {
+        if(root != null) {
+            return this.root.postOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+}
+
---
+++ 01-基础思想/src/树/二叉树/顺序存储/ArrBinaryTreeDemo.java
@@ -0,0 +1,57 @@
+package 树.二叉树.顺序存储;
+
+/**
+ * 顺序存储二叉树的特点:
+ * 1. 顺序存储二叉树 通常只考虑完全二叉树
+ * 2. 第n个元素的左子节点为 (2n +1)
+ * 3. 第n个元素的右子节点为 (2n +2)
+ * 4. 第n个元素的父子节点为 (n-1)/2
+ */
+public class ArrBinaryTreeDemo {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        //创建一个 ArrBinaryTree
+        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+        arrBinaryTree.preOrder(); // 1,2,4,5,3,6,7
+    }
+
+}
+
+//编写一个ArrayBinaryTree, 实现顺序存储二叉树遍历
+
+class ArrBinaryTree {
+    private int[] arr;//存储数据结点的数组
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    //重载preOrder
+    public void preOrder() {
+        this.preOrder(0);
+    }
+
+    //编写一个方法，完成顺序存储二叉树的 前序遍历
+
+    /**
+     * @param index 数组的下标
+     */
+    public void preOrder(int index) {
+        //如果数组为空，或者 arr.length = 0
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        //输出当前这个元素
+        System.out.println(arr[index]);
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+}
