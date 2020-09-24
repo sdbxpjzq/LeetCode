03edab72b38952105070d1e1e188ccee9e573ca7
zongqi <1165064143@qq.com>
2020-09-24 20:07:26 +0800

    update

--- 02-刷题/src/_07_树/_102_二叉树的层序遍历_中等.java
+++ 02-刷题/src/_07_树/_102_二叉树的层序遍历_中等.java
@@ -1,9 +1,6 @@
 package _07_树;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 102. 二叉树的层序遍历
@@ -47,12 +44,48 @@ public class _102_二叉树的层序遍历_中等 {
          */
 
         // 层序遍历 [3,2,8,9,10,4]
-        List<Integer> list = levelOrder(treeNode);
+        // List<Integer> list = levelOrderV(treeNode);
+        List<List<Integer>> list = levelOrder(treeNode);
         System.out.println(list);
     }
 
-    // 利用队列
-    public static List<Integer> levelOrder(TreeNode root) {
+    /**
+     * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/die-dai-di-gui-duo-tu-yan-shi-102er-cha-shu-de-cen/
+     */
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        if(root==null) {
+            return res;
+        }
+
+        //将根节点放入队列中，然后不断遍历队列
+        queue.add(root);
+        while(queue.size()>0) {
+            //获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+            int size = queue.size();
+            ArrayList<Integer> tmp = new ArrayList<Integer>();
+            //将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+            //如果节点的左/右子树不为空，也放入队列中
+            for(int i=0;i<size;++i) {
+                TreeNode t = queue.remove();
+                tmp.add(t.val);
+                if(t.left!=null) {
+                    queue.add(t.left);
+                }
+                if(t.right!=null) {
+                    queue.add(t.right);
+                }
+            }
+            //将临时list加入最终返回结果中
+            res.add(tmp);
+        }
+        return res;
+    }
+
+
+    //层序遍历 利用队列
+    public static List<Integer> levelOrderV(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         LinkedList<Integer> list = new LinkedList<>();
         if (root == null) {
