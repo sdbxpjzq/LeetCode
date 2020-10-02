70be430110285d546c12b6897a6ac87261f80c4e
zongqi <1165064143@qq.com>
2020-10-02 09:27:29 +0800

    update

---
+++ 01-基础思想/src/_06_排序/_02_常考的排序算法/_08_拓扑排序/拓扑排序.java
@@ -0,0 +1,49 @@
+package _06_排序._02_常考的排序算法._08_拓扑排序;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *应用场合
+ * 拓扑排序就是要将图论里的顶点按照相连的性质进行排序
+ *
+ * 前提
+ * 必须是有向环
+ * 图里没有环
+ *
+ */
+public class 拓扑排序 {
+    // private void sort(int[] arr) {
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     for (int i = 0; i <; i++) {
+    //
+    //     }
+    //
+    //
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 02-刷题/src/递归和回溯/递归/_247_中心对称数II_中等/Solution.java
@@ -0,0 +1,54 @@
+package 递归和回溯.递归._247_中心对称数II_中等;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 247. 中心对称数 II 中等
+ * 中心对称数是指一个数字在旋转了 180 度之后看起来依旧相同的数字（或者上下颠倒地看）。
+ *
+ * 找到所有长度为 n 的中心对称数。
+ *
+ * 示例 :
+ *
+ * 输入:  n = 2
+ * 输出: ["11","69","88","96"]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    public List<String> findStrobogrammatic(int n) {
+        if (n <= 0) {
+            return new ArrayList<>();
+        }
+        if (n == 1) {
+            return Arrays.asList("0", "1", "8");
+        }
+        if (n == 2) {
+            return Arrays.asList("11", "69", "88", "96");
+        }
+        // arr[n] 表示长度为 n 的所有中心对称数
+        List<String>[] arr = new List[n + 1];
+        arr[1] = Arrays.asList("0", "1", "8");
+        arr[2] = Arrays.asList("00","11", "69", "88", "96");
+        for (int i = 3; i <= n; i++) {
+            // 随着 i 不断地增长，我们只需要在长度为 i-2 的中心对称数两边添加 11，69，88，96 即可。
+            List<String> tmpList = new ArrayList<>();
+            for (String s : arr[i - 2]) {
+                if (i != n) {
+                    tmpList.add("0" + s + "0");
+                }
+                tmpList.add("1" + s + "1");
+                tmpList.add("6" + s + "9");
+                tmpList.add("8" + s + "8");
+                tmpList.add("9" + s + "6");
+            }
+            arr[i] = tmpList;
+        }
+        return arr[n];
+    }
+}
