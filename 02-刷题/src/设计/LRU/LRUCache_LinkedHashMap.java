ca14277f5a99bbd6473de809410f96ca1ac8aabb
zongqi <1165064143@qq.com>
2020-11-16 20:07:08 +0800

    LRU

---
+++ 02-刷题/src/设计/LRU/LRUCache_LinkedHashMap.java
@@ -0,0 +1,27 @@
+package 设计.LRU;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache_LinkedHashMap extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUCache_LinkedHashMap(int capacity) {
+        // zz true -  访问顺序 false  -  插入顺序
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
