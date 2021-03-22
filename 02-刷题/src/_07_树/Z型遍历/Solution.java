8782cf3c6626469374119ec93142df74d1482aeb
zongqi <1165064143@qq.com>
2021-03-22 14:05:32 +0800

    add

--- 02-刷题/src/_07_树/Z型遍历/Solution.java
+++ 02-刷题/src/_07_树/Z型遍历/Solution.java
@@ -5,39 +5,36 @@ import _07_树.构建二叉树.CreateBT;
 import java.util.*;
 
 public class Solution {
-    static List<List<Integer>> res = new ArrayList<>();
-    public void zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        // 遍历标志
-        int flag = 0;
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            LinkedList<Integer> tmp = new LinkedList<>();
-            while (size-- > 0) {
-                TreeNode node = queue.poll();
-                tmp.add(node.val);
-                if (flag == 0) {
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
-                } else {
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
-                }
+    List<List<Integer>> res = new ArrayList<>();
 
-            }
-            // 标记下一层处理的方向
-            flag = 1 - flag;
-            res.add(tmp);
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return res;
         }
-        System.out.println(res);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int flag = 0;
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (flag == 0) {
+                    tmp.add(node.val);
+                } else {
+                    // 头插法
+                    tmp.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(tmp);
+            flag = 1-flag;
+        }
+        return res;
     }
 }
