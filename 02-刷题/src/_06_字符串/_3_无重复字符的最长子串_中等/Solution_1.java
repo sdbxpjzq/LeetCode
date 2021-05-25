335bb973e2b23fada4552a391bcf53a2967662bd
zongqi <zongqi@yidian-inc.com>
2021-05-25 09:57:43 +0800

    add

--- 02-刷题/src/_06_字符串/_3_无重复字符的最长子串_中等/Solution_1.java
+++ 02-刷题/src/_06_字符串/_3_无重复字符的最长子串_中等/Solution_1.java
@@ -3,16 +3,17 @@ package _06_字符串._3_无重复字符的最长子串_中等;
 import java.util.HashSet;
 
 public class Solution_1 {
-    public int lengthOfLongestSubstring2(String s) {
+    public  int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
         HashSet<Character> set = new HashSet<>();
         int start = 0, end = 0, maxLen = 0;
         while (end < s.length()) {
-            if (!set.contains(s.charAt(end))) {
-                set.add(s.charAt(end));
+            if (!set.contains(chars[end])) {
+                set.add(chars[end]);
                 end++;
                 maxLen = Math.max(maxLen, set.size());
             } else {
-                set.remove(s.charAt(start));
+                set.remove(chars[start]);
                 start++;
             }
         }
