9766468b7f3981edc1612f193e0bf1158fe0f725
zongqi <zongqi@yidian-inc.com>
2021-06-26 18:10:52 +0800

    add

--- 02-刷题/src/_06_字符串/最长公共子序列_中等/Solution.java
+++ 02-刷题/src/_06_字符串/最长公共子序列_中等/Solution.java
@@ -10,15 +10,15 @@ public class Solution {
         System.out.println(i);
 
     }
-    public static int maxLong(String str1, String str2) {
-        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {
+    public static int maxLong(String text1, String text2) {
+        if (text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
             return 0;
         }
-        char[] str1Arr = str1.toCharArray();
-        char[] str2Arr = str2.toCharArray();
+        char[] str1Arr = text1.toCharArray();
+        char[] str2Arr = text2.toCharArray();
         int maxLen = 0;
         ArrayList<Character> list = new ArrayList<>();
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
         for (int i = 1; i <= str1Arr.length; i++) {
             for (int j = 1; j <= str2Arr.length; j++) {
                 if (str1Arr[i-1] == str2Arr[j-1]) {
